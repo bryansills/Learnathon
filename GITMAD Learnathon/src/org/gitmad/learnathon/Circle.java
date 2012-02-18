a16a3d26ffe8a53cea953dac92cad45312ec4ea6
Bryan Sills <bryan.a.sills@gmail.com>
2012-02-18 16:12:41 -0500

    Forgot a file

---
+++ GITMAD Learnathon/src/org/gitmad/learnathon/Circle.java
@@ -0,0 +1,55 @@
+package org.gitmad.learnathon;
+
+public class Circle {
+	private float x, y;
+	private int time;
+	
+	public Circle(float x, float y, int time) {
+		this.setX(x);
+		this.setY(y);
+		this.setTime(time);
+	}
+
+	/**
+	 * @return the x
+	 */
+	public float getX() {
+		return x;
+	}
+
+	/**
+	 * @param x the x to set
+	 */
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	/**
+	 * @return the y
+	 */
+	public float getY() {
+		return y;
+	}
+
+	/**
+	 * @param y the y to set
+	 */
+	public void setY(float y) {
+		this.y = y;
+	}
+
+	/**
+	 * @return the time
+	 */
+	public int getTime() {
+		return time;
+	}
+
+	/**
+	 * @param time the time to set
+	 */
+	public void setTime(int time) {
+		this.time = time;
+	}
+
+}
