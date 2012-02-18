c0f31a0f5aa4af6c106a94befa1dac6786d8944d
Bryan Sills <bryan.a.sills@gmail.com>
2012-02-17 21:52:12 -0500

    initial commit

---
+++ GITMAD Learnathon/src/org/gitmad/learnathon/GITMADLearnathonActivity.java
@@ -0,0 +1,13 @@
+package org.gitmad.learnathon;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GITMADLearnathonActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
