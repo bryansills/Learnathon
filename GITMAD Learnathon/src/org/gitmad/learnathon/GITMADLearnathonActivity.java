bee0589f3c5c0a031cc6e9815e4e1e60583f1284
Bryan Sills <bryan.a.sills@gmail.com>
2012-02-18 15:17:45 -0500

    Added touchListener

--- GITMAD Learnathon/src/org/gitmad/learnathon/GITMADLearnathonActivity.java
+++ GITMAD Learnathon/src/org/gitmad/learnathon/GITMADLearnathonActivity.java
@@ -2,6 +2,9 @@ package org.gitmad.learnathon;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 
 public class GITMADLearnathonActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,18 @@ public class GITMADLearnathonActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        final Panel panel = (Panel)findViewById(R.id.SurfaceView01);
+        
+        OnTouchListener listener = new OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				float xCo = event.getX();
+				float yCo = event.getY();
+//				int time = (int) event.getHistorySize();
+				panel.addCircle(xCo, yCo);
+				return true;
+			}
+        };
+        panel.setOnTouchListener(listener);
     }
 }
--- GITMAD Learnathon/src/org/gitmad/learnathon/Panel.java
+++ GITMAD Learnathon/src/org/gitmad/learnathon/Panel.java
@@ -1,9 +1,12 @@
 package org.gitmad.learnathon;
 
+import java.util.ArrayList;
+
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PointF;
 import android.util.AttributeSet;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -12,12 +15,16 @@ import android.view.SurfaceView;
 
 public class Panel extends SurfaceView implements SurfaceHolder.Callback{
 	private CanvasThread canvasthread;
+	private ArrayList<PointF> circleList;
+	private ArrayList<Circle> circleSizeList;
 	
     public Panel(Context context, AttributeSet attrs) {
 		super(context, attrs); 
 	    getHolder().addCallback(this);
 	    canvasthread = new CanvasThread(getHolder(), this);
 	    setFocusable(true);
+	    circleList = new ArrayList<PointF>();
+	    circleSizeList = new ArrayList<Circle>();
 	}
 
 	public Panel(Context context) {
@@ -26,13 +33,6 @@ public class Panel extends SurfaceView implements SurfaceHolder.Callback{
 		canvasthread = new CanvasThread(getHolder(), this);
 		setFocusable(true);
     }
-
-	@Override
-	public void onDraw(Canvas canvas) {
-		Paint paint = new Paint();
-	    paint.setColor(Color.YELLOW);
-	    canvas.drawCircle(50, 50, 25, paint);
-	}
 	 
 	public void surfaceChanged(SurfaceHolder holder, int format, int width,
 			int height) {
@@ -54,5 +54,23 @@ public class Panel extends SurfaceView implements SurfaceHolder.Callback{
 				// we will try it again and again...
 			}
 		}
+	}
+
+	@Override
+	public void onDraw(Canvas canvas) {
+		Paint paint = new Paint();
+	    paint.setColor(Color.YELLOW);
+
+	    for (PointF circle: circleList) {
+	    	canvas.drawCircle(circle.x, circle.y, 20, paint);
+	    }
+	}
+
+	public void addCircle(float xCo, float yCo) {
+		circleList.add(new PointF(xCo, yCo));
+	}
+
+	public void addSizeCircle(float xCo, float yCo, int time) {
+		circleSizeList.add(new Circle(xCo, yCo, time));
 	}
 }   
